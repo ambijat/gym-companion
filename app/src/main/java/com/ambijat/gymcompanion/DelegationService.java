15dc2caf3ea94e105043e1eaab310af9e21c6c1e
ambijat <ambijat@gmail.com>
2026-06-18 11:35:32 +0530

    chore(play): add generated Android TWA project files

---
+++ app/src/main/java/com/ambijat/gymcompanion/DelegationService.java
@@ -0,0 +1,14 @@
+package com.ambijat.gymcompanion;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
